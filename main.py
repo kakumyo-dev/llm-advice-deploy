91138d1d86f6ff0ff11c75cce89ca4f06b52cd56
kakumyo-dev <m.kakumyo.dev@gmail.com>
2025-06-08 09:41:51 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from google.cloud import bigquery
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    try:
+        client = bigquery.Client()
+        query = """
+            SELECT *
+            FROM `dev.syacho-kojin`
+            LIMIT 10
+        """
+        query_job = client.query(query)
+        results = query_job.result()
+
+        data = [dict(row.items()) for row in results]
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
